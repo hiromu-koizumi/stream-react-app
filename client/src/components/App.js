57fe3c8c205acb8e71a2e5f9873b03cd200505bf
小泉大夢 <>
2019-08-17 23:33:57 +0900

    create: history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter,Route,Link} from 'react-router-dom';
+import {Router,Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 
 
@@ -13,7 +14,7 @@ import Header from './Header';
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
             <div>
             <Header/>
             <Route path="/" exact component={StreamList}/>
@@ -22,7 +23,7 @@ const App = () => {
             <Route path="/stream/delete" exact component={StreamDelete}/>
             <Route path="/stream/show" exact component={StreamShow}/>
             </div>
-            </BrowserRouter>
+            </Router>
         </div>
     )
 };
